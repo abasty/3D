e3ad593f01fb8ec8b60231bb60e014daa3e88c5d
Alain Basty <alain.basty@free.fr>
2024-02-25 18:02:49 +0100

    Added WorkdEnvironment

---
+++ README.md
@@ -0,0 +1,11 @@
+# Todo List
+
+- [x] Atmospherics : Ciel, Soleil
+- [ ] Terrain avec fog
+- [ ] Cockpit 3D, render on texture
+
+# Liens
+
+# Modèles 3D
+
+- [CG Trader](https://www.cgtrader.com/search?free=1&keywords=space)
--- test.gd
+++ test.gd
@@ -1,24 +1,24 @@
 extends Node3D
 
-const delta = 2
+const camera_speed = 4
 
-@onready var camera = $Camera
-@onready var light = $Light
-@onready var camera_azimuth = 0
+@onready var camera : Camera3D = $Camera
+@onready var light : DirectionalLight3D = $Light
+@onready var camera_azimuth := 0
 
 # Add input handling
-func _input(event):
-	var camera_last_azimuth = camera_azimuth
+func _input(event: InputEvent) -> void:
+	var camera_last_azimuth := camera_azimuth
 	if event is InputEventKey:
 		if event.keycode == KEY_LEFT && event.pressed:
-			camera_azimuth -= delta
+			camera_azimuth -= camera_speed
 		elif event.keycode == KEY_RIGHT && event.pressed:
-			camera_azimuth += delta
+			camera_azimuth += camera_speed
 		if event.keycode == KEY_UP && event.pressed:
-			camera.position.y += delta
+			camera.position.y += camera_speed
 		elif event.keycode == KEY_DOWN && event.pressed:
-			camera.position.y -= delta
-			
+			camera.position.y -= camera_speed
+
 		if camera_azimuth != camera_last_azimuth:
 			camera.position.x = cos(deg_to_rad(camera_azimuth)) * 50
 			camera.position.z = sin(deg_to_rad(camera_azimuth)) * 50
@@ -28,6 +28,6 @@ func _input(event):
 func _ready():
 	camera.look_at(Vector3(0, 0, 0))
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Called every frame. 'camera_speed' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
